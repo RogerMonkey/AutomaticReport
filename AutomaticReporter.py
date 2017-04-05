70d564d41a57bf703573feab5f61e85bed9231b4
RogerMonkey <gufengoyx@qq.com>
2017-04-05 20:52:32 +0800

    123

--- AutomaticReporter.py
+++ AutomaticReporter.py
@@ -1,11 +1,10 @@
 from wxpy import *
 import datetime
-# from tinydb import TinyDB, Query
-# import arrow
+
 from apscheduler.schedulers.background import BackgroundScheduler
 
-bot, tuling = Bot(cache_path=True), Tuling(api_key='4c7e082b6db749489d7ff691fb9c863b')
-# 去http://www.tuling123.com 申请你自己的api_key
+apikey = open('config').readline().strip()
+bot, tuling = Bot(cache_path=True), Tuling(api_key=apikey)
 
 friends, groups, reportTo = [], ["一个亿的策马奔腾"], ["一个亿的策马奔腾"]
 
@@ -22,19 +21,11 @@ def searchFriends(friend_names, type_name):
 
 
 def report():
-    # db = TinyDB('dailyReport.json')
-    # table = db.table(arrow.utcnow().to('local').format('YYYY-MM-DD')).all()
-    # if len(table) == 0:
-    #     print(arrow.utcnow().to('local').format('YYYY-MM-DD H:M:S'), "发送日报失败")
-    #     return
     r = open('report.txt', 'r')
     contents = ''.join(r.readlines())
     print(contents)
-    # table = table[0]
-    # contents = table["title"] + '\n' + table["contents"]
     for recipient in searchFriends(reportTo,"groups"):
         recipient.send(contents)
-    # print(arrow.utcnow().to('local').format('YYYY-MM-DD H:M:S'), "发送一次日报")
 
 
 @bot.register(searchFriends(friends, "friends") + searchFriends(groups, "groups"), TEXT)
@@ -42,7 +33,7 @@ def auto_reply(msg):
     if isinstance(msg.chat, Group) and not msg.is_at:
         return
     else:
-        msg.reply(tuling.reply_text(msg) + "\n---------------自动回复")
+        msg.reply(tuling.reply_text(msg))
 
 
 scheduler.add_job(report, 'cron', hour=20, minute=40, id="dailyReport")
--- README.md
+++ README.md
@@ -1,15 +1,11 @@
 # AutomaticReport
 一段代码片段，用来每天自动使用微信向老板发日报，搬砖真苦！
-# 运行环境
-建议使用Python3，Python2没测过
 
 # 依赖
 1. wxpy
-2. arrow
-3. tinydb
-4. apscheduler
+2. apscheduler
+
 # 使用方法
-- 使用insertDailyReport往文档型数据库中写入每天要准备发送的日报
 - 使用scheduler.add_job中的hour 和 minute参数设置每天发送的日报时间，取值范围分别为[0,23],[0,59]
 - 程序会自动读取对应日期的数据进行发送，如果没有对应日的数据，程序不会自行发送
 - **如果要明天发送今天的日报，注意自行将日报时间减一**
